b3d66d9bd54bdc4c02c241d982e4b6fc5b8caae0
boaft <troischdamian@gmail.com>
2022-04-04 22:01:01 +0930

    major breakthrough

--- startup.lua
+++ startup.lua
@@ -1,7 +1,5 @@
 --TODO
 --https://en.wikipedia.org/wiki/Descriptive_notation
---Make array of all possible squares using get touch
---Make setup pieces function
 --Make move logic
 --Make timers and start game **players both hit start on thier screens within 3 seconds
 local function round(num, mult)
@@ -58,34 +56,15 @@ function setup()
     {81,15},{81,30},{81,45},{81,60},{81,76},{81,91},{81,106},{81,121}
   }
   memory={
-    {"Brook"},{"Bknight"},{"Bbishop"},{"Bqueen"},{"Bking"},{"Bbishop"},{"Bknight"},{"Brook"},
-    {"Bpawn"},{"Bpawn"},{"Bpawn"},{"Bpawn"},{"Bpawn"},{"Bpawn"},{"Bpawn"},{"Bpawn"},
-    {"X"},{"X"},{"X"},{"X"},{"X"},{"X"},{"X"},{"X"},
-    {"X"},{"X"},{"X"},{"X"},{"X"},{"X"},{"X"},{"X"},
-    {"X"},{"X"},{"X"},{"X"},{"X"},{"X"},{"X"},{"X"},
-    {"X"},{"X"},{"X"},{"X"},{"X"},{"X"},{"X"},{"X"},
-    {"Wpawn"},{"Wpawn"},{"Wpawn"},{"Wpawn"},{"Wpawn"},{"Wpawn"},{"Wpawn"},{"Wpawn"},
-    {"Wrook"},{"Wknight"},{"Wbishop"},{"Wqueen"},{"Wking"},{"Wbishop"},{"Wknight"},{"Wrook"}
+    {"Brook","Bknight","Bbishop","Bqueen","Bking","Bbishop","Bknight","Brook"},
+    {"Bpawn","Bpawn","Bpawn","Bpawn","Bpawn","Bpawn","Bpawn","Bpawn"},
+    {"X","X","X","X","X","X","X","X"},
+    {"X","X","X","X","X","X","X","X"},
+    {"X","X","X","X","X","X","X","X"},
+    {"X","X","X","X","X","X","X","X"},
+    {"Wpawn","Wpawn","Wpawn","Wpawn","Wpawn","Wpawn","Wpawn","Wpawn"},
+    {"Wrook","Wknight","Wbishop","Wqueen","Wking","Wbishop","Wknight","Wrook"}
   }
-end
-
-function player_selection()
-  flag=0
-  while true do
-    event, side, xPos, yPos = os.pullEvent("monitor_touch") 
-    x=math.floor(xPos/15)+1
-    y=math.ceil(yPos/10)
-    if x==5 and y~=5 then
-      screen:drawRect((squares[x][2])-16, (squares[y*8][1])-10, 16, 10,colors.red)
-    elseif y==5 and x~=5 then
-      screen:drawRect((squares[x][2])-15, (squares[y*8][1])-11, 15, 11,colors.red)
-    elseif y==5 and x==5 then
-      screen:drawRect((squares[x][2])-16, (squares[y*8][1])-11, 16, 11,colors.red)
-    else
-      screen:drawRect((squares[x][2])-15,(squares[y*8][1])-10,15,10,colors.red)
-    end
-    screen:output()
-  end
 end
 
 function pieces_setup()
@@ -109,8 +88,126 @@ function pieces_setup()
     screen:drawSurface(pawn_white,(squares[i][2])-10,(squares[8*7][1])-6)
     screen:drawSurface(pawn_black,(squares[i][2])-10,(squares[8*2][1])-7)
     screen:output()
-    os.sleep(0.25)
   end
+end
+
+function is_piece(x,y)
+  if memory[y][x]=="X" then
+    return false
+  else
+    return true
+  end
+end
+function what_color(x,y)
+  if string.sub(memory[y][x],1,1)=="B" then
+    return "B"
+  else
+    return "W"
+  end
+end
+function is_opponent(x,y,turn)
+  if is_piece(x,y)==true then
+    if turn=="W" then
+      if string.sub(memory[y][x],1,1)=="B" then
+        return true
+      else
+        return false
+      end
+    else
+      if string.sub(memory[y][x],1,1)=="W" then
+        return true
+      else
+        return false
+      end
+    end
+  end
+end
+
+
+function fix_square_color(x,y)
+  if y%2==0 and x%2~=0 or y%2~=0 and x%2==0 then
+    if x==5 and y~=5 then
+      screen:drawRect((squares[x][2])-16, (squares[y*8][1])-10, 16, 10,colors.black)
+    elseif y==5 and x~=5 then
+      screen:drawRect((squares[x][2])-15, (squares[y*8][1])-11, 15, 11,colors.black)
+    elseif y==5 and x==5 then
+      screen:drawRect((squares[x][2])-16, (squares[y*8][1])-11, 16, 11,colors.black)
+    else
+      screen:drawRect((squares[x][2])-15,(squares[y*8][1])-10,15,10,colors.black)
+    end
+  elseif y%2~=0 and x%2~=0 or y%2==0 and x%2==0 or y==x then
+    if x==5 and y~=5 then
+      screen:drawRect((squares[x][2])-16, (squares[y*8][1])-10, 16, 10,colors.white)
+    elseif y==5 and x~=5 then
+      screen:drawRect((squares[x][2])-15, (squares[y*8][1])-11, 15, 11,colors.white)
+    elseif y==5 and x==5 then
+      screen:drawRect((squares[x][2])-16, (squares[y*8][1])-11, 16, 11,colors.white)
+    else
+      screen:drawRect((squares[x][2])-15,(squares[y*8][1])-10,15,10,colors.white)
+    end
+  end
+end
+
+function square_refresh(x,y)
+  if y%2==0 and x%2~=0 or y%2~=0 and x%2==0 then
+    if x==5 and y~=5 then
+      screen:fillRect((squares[x][2])-16, (squares[y*8][1])-10, 16, 10,colors.black)
+    elseif y==5 and x~=5 then
+      screen:fillRect((squares[x][2])-15, (squares[y*8][1])-11, 15, 11,colors.black)
+    elseif y==5 and x==5 then
+      screen:fillRect((squares[x][2])-16, (squares[y*8][1])-11, 16, 11,colors.black)
+    else
+      screen:fillRect((squares[x][2])-15,(squares[y*8][1])-10,15,10,colors.black)
+    end
+  elseif y%2~=0 and x%2~=0 or y%2==0 and x%2==0 or y==x then
+    if x==5 and y~=5 then
+      screen:fillRect((squares[x][2])-16, (squares[y*8][1])-10, 16, 10,colors.white)
+    elseif y==5 and x~=5 then
+      screen:fillRect((squares[x][2])-15, (squares[y*8][1])-11, 15, 11,colors.white)
+    elseif y==5 and x==5 then
+      screen:fillRect((squares[x][2])-16, (squares[y*8][1])-11, 16, 11,colors.white)
+    else
+      screen:fillRect((squares[x][2])-15,(squares[y*8][1])-10,15,10,colors.white)
+    end
+  end
+  screen:output()
+end
+
+function memory_update(old_x,old_y,x,y)
+  temp=memory[old_y][old_x]
+  memory[y][x]=temp
+  memory[old_y][old_x]="X"
+end
+
+function update_piece(old_x,old_y,x,y)
+  square_refresh(old_x,old_y)
+  square_refresh(x,y)
+  if memory[y][x]=="Wpawn" then
+    screen:drawSurface(pawn_white,(squares[x][2])-10,(squares[8*y][1])-6)
+  elseif memory[y][x]=="Bpawn" then
+    screen:drawSurface(pawn_black,(squares[x][2])-10,(squares[8*y][1])-6)
+  elseif memory[y][x]=="Brook" then
+    screen:drawSurface(rook_black,(squares[x][2])-10,(squares[8*y][1])-6)
+  elseif memory[y][x]=="Wrook" then
+    screen:drawSurface(rook_white,(squares[x][2])-10,(squares[8*y][1])-6)
+  elseif memory[y][x]=="Bknight" then
+    screen:drawSurface(knight_black,(squares[x][2])-10,(squares[8*y][1])-6)
+  elseif memory[y][x]=="Wknight" then
+    screen:drawSurface(knight_white,(squares[x][2])-10,(squares[8*y][1])-6)
+  elseif memory[y][x]=="Bbishop" then
+    screen:drawSurface(bishop_black,(squares[x][2])-10,(squares[8*y][1])-6)
+  elseif memory[y][x]=="Wbishop" then
+    screen:drawSurface(bishop_white,(squares[x][2])-10,(squares[8*y][1])-6)
+  elseif memory[y][x]=="Wking" then
+    screen:drawSurface(king_white,(squares[x][2])-10,(squares[8*y][1])-6)
+  elseif memory[y][x]=="Bking" then
+    screen:drawSurface(king_black,(squares[x][2])-10,(squares[8*y][1])-6)
+  elseif memory[y][x]=="Wqueen" then
+    screen:drawSurface(queen_white,(squares[x][2])-10,(squares[8*y][1])-6)
+  elseif memory[y][x]=="Bqueen" then
+    screen:drawSurface(queen_black,(squares[x][2])-10,(squares[8*y][1])-6)
+  end
+  screen:output()
 end
 
 function board_refresh()
@@ -119,12 +216,51 @@ function board_refresh()
   screen:output()
 end
 
+function player_selection(turn)
+  flag=0
+  old_x=1
+  old_x=1
+  repeat
+    if flag==1 then
+      old_x=x
+      old_y=y
+      fix_square_color(old_x,old_y)
+    end
+    event, side, xPos, yPos = os.pullEvent("monitor_touch") 
+    x=math.floor(xPos/15)+1
+    y=math.ceil(yPos/10)
+    if y==9 then
+      y=8
+    end
+    if is_piece(x,y)==true and what_color(x,y)==turn then
+      if x==5 and y~=5 then
+        screen:drawRect((squares[x][2])-16, (squares[y*8][1])-10, 16, 10,colors.red)
+        flag=1
+      elseif y==5 and x~=5 then
+        screen:drawRect((squares[x][2])-15, (squares[y*8][1])-11, 15, 11,colors.red)
+        flag=1
+      elseif y==5 and x==5 then
+        screen:drawRect((squares[x][2])-16, (squares[y*8][1])-11, 16, 11,colors.red)
+        flag=1
+      else
+        screen:drawRect((squares[x][2])-15,(squares[y*8][1])-10,15,10,colors.red)
+        flag=1
+      end
+      screen:output()
+    end
+  until((is_piece(x,y)==false or is_opponent(x,y,turn)==true) and is_piece(old_x,old_y)==true and flag==1)
+  memory_update(old_x,old_y,x,y)
+  update_piece(old_x,old_y,x,y)
+end
+
 function test()
   board_refresh()
   pieces_setup()
   --screen:drawSurface(pawn_white,squares[1][2]-10,squares[8*6][1]-6)
-  player_selection()
-
+  while true do
+    player_selection("W")
+    player_selection("B")
+  end
 end
 
 setup()
